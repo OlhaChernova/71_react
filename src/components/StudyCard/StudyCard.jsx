fe254a0642aba6bf261acbd3a6fe1bb45c96fa6a
OlhaChernova1987 <chernova.olha1987@gmail.com>
2025-11-04 13:33:58 +0100

    add lesson03

---
+++ src/components/StudyCard/StudyCard.jsx
@@ -0,0 +1,16 @@
+import Button from "../Button/Button";
+import "./styles.css";
+
+function StudyCard({lessonData}) {
+  return (
+    <div className="study-card-wrapper">
+      <h3>Lessson {lessonData.lesson_number}</h3>
+      <p>{lessonData.topic}</p>
+      <p>{lessonData.name}</p>
+      <img src={lessonData.imgSrc}/>
+      <Button name="view" type="button"/>
+    </div>
+  );
+}
+
+export default StudyCard;
