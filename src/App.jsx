3a94fe534824b7e6d7e74f749a5664995ec1c961
OlhaChernova1987 <chernova.olha1987@gmail.com>
2025-11-06 12:37:43 +0100

    add homework04

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@
 // Импорты лекций
 // import Lesson02 from "./lessons/Lesson02/Lesson02";
 // import Lesson03 from "./lessons/Lesson03/Lesson03";
-import Lesson04 from "./lessons/Lesson04/Lesson04";
+
+// import Lesson04 from "./lessons/Lesson04/Lesson04";
 
 // Импорты домашних работ
 // import Homework02 from "./homeworks/Homework02/Homework02";
 // import Homework03 from "./homeworks/Homework03/Homework03";
+import Homework04 from "./homeworks/Homework04/Homework04";
 
 function App() {
   return (
@@ -28,8 +30,8 @@ function App() {
       {/* <Homework03 /> */}
 
       {/* Лекция 4 */}
-      <Lesson04 />
-      {/* <Homework04 /> */}
+      {/* <Lesson04 /> */}
+      <Homework04 />
     </>
   );
 }
